be74a8bfd9a76f5ce83fe2d1fa741b47a6a1b3fb
huangbaiwei <504451409@qq.com>
2017-03-25 19:08:28 +0800

    bubble2

--- bubble.c
+++ bubble.c
@@ -1,3 +1,4 @@
+//普通方法
 void bubble(int a[],int n)
 {
 	int tmp;
@@ -15,9 +16,32 @@ void bubble(int a[],int n)
 	}
 }
 
+//优化1
+void bubble1(int a[],int n)
+{
+
+	int tmp,flag;
+	for(int i=0;i<n-1;i++)
+	{
+		flag = 0;
+		for(int j=0;j<n-1-i;j++)
+		{
+			if(a[i]>a[j])
+			{
+				flag = 1;
+				tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+			}
+		}
+		if(flag == 0)
+			break;
+	}
+}
+
 int main()
 {
 	int arr[] = {3,5,5,7,2,8,4};
-	bubble(arr,sizeof(arr)/sizeof(arr[0]));
+	bubble1(arr,sizeof(arr)/sizeof(arr[0]));
 	return 0;
 }
